16d19dd58ada9f3c9e2a7140b37de0c9b2842fee
Khuê Nguyễn <19521712@ms.uit.edu.vn>
2021-10-04 19:03:26 +0700

    Change color icon

--- Foody/Foody.Android/Renderers/SetProperties.cs
+++ Foody/Foody.Android/Renderers/SetProperties.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Content.Res;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -24,21 +25,65 @@ namespace Foody.Droid.Renderers
 
         public void ResetAppearance(BottomNavigationView bottomView)
         {
+            
 
+            var parameters = bottomView.LayoutParameters;
+            bottomView.ItemTextAppearanceActive = Android.Graphics.Color.Red;
+            //bottomView.ItemIconTintList = myList;
+            parameters.Height = 200;
+            bottomView.LayoutParameters = parameters;
         }
 
         public void SetAppearance(BottomNavigationView bottomView, IShellAppearanceElement appearance)
         {
 
+            //int[][] states = new int[][]
+            // {
+            //    new int[] { Android.Resource.Attribute.StateEnabled}, // enabled
+            //    new int[] {-Android.Resource.Attribute.StateEnabled}, // disabled
+            //    new int[] {-Android.Resource.Attribute.StateChecked}, // unchecked
+            //    new int[] { Android.Resource.Attribute.StateChecked }  // pressed
+            // };
 
+            //int[] colors = new int[]
+            //{
+            //    Xamarin.Forms.Color.Black.ToAndroid(),
+            //    Xamarin.Forms.Color.Red.ToAndroid(),
+            //    Xamarin.Forms.Color.Green.ToAndroid(),
+            //    Xamarin.Forms.Color.Blue.ToAndroid(),
+            //    Xamarin.Forms.Color.Blue.ToAndroid(),
+            //};
 
-            //bottomView.SetBackgroundColor(Android.Graphics.Color.White);
+            //ColorStateList myList = new ColorStateList(states, colors);
+
+            //bottomView.SetBackgroundColor(Android.Graphics.Color.ParseColor("#487eb0"));
             // bottomView.SetBackgroundResource(Resource.Drawable.bottombackground);
+
+            //bottomView.SetBackgroundColor(Android.Graphics.Color.ParseColor("#487eb0"));
+
+            var parameters = bottomView.LayoutParameters;
+            bottomView.ItemTextAppearanceActive = (global::Android.Graphics.Color.LightGreen);
+            parameters.Height = 200;
+            bottomView.LayoutParameters = parameters;
+
+            // Change colors on different states
+            int[][] states = new int[][]{
+            new int[]{-Android.Resource.Attribute.StateChecked},  // unchecked
+            new int[]{Android.Resource.Attribute.StateChecked},   // checked
+            new int[]{}                                // default
+    };
+
+            int[] colors = new int[]{
+            Android.Graphics.Color.ParseColor("#747474"),
+            Android.Graphics.Color.ParseColor("#007f42"),
+            Android.Graphics.Color.ParseColor("#747474"),
+    };
+            ColorStateList navigationViewColorStateList = new ColorStateList(states, colors);
+            bottomView.ItemIconTintList = navigationViewColorStateList;
+            bottomView.ItemTextColor = navigationViewColorStateList;
             bottomView.SetPadding(10, 5, 5, 10);
             bottomView.SetShiftMode(true, true);
   
-
-
         }
 
     }
