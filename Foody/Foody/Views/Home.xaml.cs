1dc04e94f8442701aa8b7ef18e76805bea64c0e7
Khuê Nguyễn <19521712@ms.uit.edu.vn>
2021-10-06 02:00:55 +0700

    Dinh kouttttttttt

--- Foody/Foody/Views/Home.xaml.cs
+++ Foody/Foody/Views/Home.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,10 +13,34 @@ namespace Foody.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Home : ContentPage
     {
+
+        public ICommand NavToPantry => new Command(NavToPantryPage);
+        public ICommand NavToShoppingList => new Command(NavToShoppingListPage);
+        public ICommand NavToMealPlanner => new Command(NavToMealPlannerPage);
         public Home()
         {
             InitializeComponent();
+            BindingContext = this;
            
         }
+
+        async public void NavToPantryPage()
+        {
+           
+            await (App.Current.MainPage as Xamarin.Forms.Shell).GoToAsync("//tabbar/pantry", true);
+        }
+
+        async public void NavToShoppingListPage()
+        {
+
+            await (App.Current.MainPage as Xamarin.Forms.Shell).GoToAsync("//tabbar/shoppingList", true);
+        }
+        async public void NavToMealPlannerPage()
+        {
+
+            await (App.Current.MainPage as Xamarin.Forms.Shell).GoToAsync("//tabbar/mealPlanner", true);
+        }
+
+
     }
 }
