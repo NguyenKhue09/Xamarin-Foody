28fcbc866232d47bea8c655ea06439fad802dfe3
NguyenTraVi <19522512@gm.uit.edu.vn>
2021-10-14 00:48:50 +0700

    buggg

--- Foody/Foody/Views/DetailsRecipe/DetailRecipe.xaml.cs
+++ Foody/Foody/Views/DetailsRecipe/DetailRecipe.xaml.cs
@@ -34,17 +34,8 @@ namespace Foody.Views.DetailsRecipe
             }
         }
 
-        private void TabHost_SelectedTabIndexChanged(object sender, SelectedPositionChangedEventArgs e)
-        {
-            if (TabHost.SelectedIndex != 0 )
-            {
-                faketab1.IsVisible = false;
-            }
-        }
+       
 
-        private void scroll_SizeChanged(object sender, EventArgs e)
-        {
-        }
-            
+         
     }
 }
