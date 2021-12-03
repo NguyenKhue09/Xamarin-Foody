843f39b9347632c248c823ae4db9050390a05519
NguyenTraVi <19522512@gm.uit.edu.vn>
2021-12-03 22:07:27 +0700

    ...

--- Foody/Foody/Constants/Constants.cs
+++ Foody/Foody/Constants/Constants.cs
@@ -13,9 +13,9 @@ namespace Foody.Constants
         public static int  NUMBER = 50;
         //public static string APIKEY = "757a905978594ef086e312509e14e747";
         //public static string APIKEY = "654a09cc5b3549ab9d6442ccd23f9ef3";
-        public static string APIKEY = "fb0553d9de4949eb9494d7a43de9f60c";
+        //public static string APIKEY = "fb0553d9de4949eb9494d7a43de9f60c";
         // public static string APIKEY = "e8096985f30b45bf8e3212115a218cc5";
-        //public static string APIKEY = "8742b6ff28af45379a5f06320dba113b";
+        public static string APIKEY = "8742b6ff28af45379a5f06320dba113b";
         //public static string APIKEY = "409407cdfa02474f8e61e7f643a7dbec";
         //public static string APIKEY = "85e55bb723d44cffbbfcccd99a68225d";
         //public static string APIKEY = "ef20217a3df647f8ae51ed4271f68858";
