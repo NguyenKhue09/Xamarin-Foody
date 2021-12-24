1be9ef871d90aa2bd25d289f92a914b2d3f95406
Khuê Nguyễn <19521712@ms.uit.edu.vn>
2021-12-25 02:23:14 +0700

    add popup for meal plan

---
+++ Foody/Foody/Views/PopUp/MealPlanPopup.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Foody.Views.PopUp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MealPlanPopup : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public MealPlanPopup()
+        {
+            InitializeComponent();
+            this.CloseWhenBackgroundIsClicked = false;
+        }
+
+        public async Task closeMealPlanPopup()
+        {
+            await Navigation.PopPopupAsync();
+        }
+    }
+}
